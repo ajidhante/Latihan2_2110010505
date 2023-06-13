6fb663c11137e61a8a0a007988cadafc0fe34e68
ajidhante <babung123456789@gmail.com>
2023-06-13 09:50:15 +0800

    Menjalankan Program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,5 +1,18 @@
 package universitas;
 
 public class MahasiswaBeraksi {
-    
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        
+        mahasiswa1.npm = "2110010505";
+        mahasiswa1.nama = "Fadli";
+        mahasiswa1.prodi = "TI";
+        
+        System.out.println("Constructor");
+        System.out.println("NPM : "+mahasiswa1.npm);
+        System.out.println("Nama : "+mahasiswa1.nama);
+        System.out.println("Prodi : "+mahasiswa1.prodi);
+
+        
+    }
 }
